a30ce1d54046d8f1aed89b829dc36eb977ab4b80
Matheus Araujo <matheus.a@havan.com.br>
2022-04-01 16:13:32 -0300

    Criando Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import type { NextPage } from "next";
+import { Flex, Stack, Button, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from "../Components/Input";
 
-const Home: NextPage = () => {
-  return <h1>Projeto Criado</h1>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="E-mail" />
+          <Input name="password" label="Password" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
