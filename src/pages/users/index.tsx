32e2e5490c8f9271b8f1e75907bcbed31015ada9
Matheus Araujo <matheus.a@havan.com.br>
2022-04-08 15:47:57 -0300

    Pagina: Lista de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,93 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../Components/Header";
+import { Sidebar } from "../../Components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            {/* Cabeçalho da Tabela */}
+            <Thead>
+              {/* Linha */}
+              <Tr>
+                {/* Título de uma coluna */}
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuários</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            {/* Corpo da Tabela */}
+            <Tbody>
+              <Tr>
+                <Td>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Matheus dwe Araújo Leite</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      matheusaraujo.ti10@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text fontWeight="bold">21 de Fevereiro</Text>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
