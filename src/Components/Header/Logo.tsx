e473e00e655e2a9b8e784bf16b6888d24678fb0b
Matheus <matheusaraujo.ti10@gmail.com>
2022-04-10 13:18:51 -0300

    Responsividade no Chakra

--- src/Components/Header/Logo.tsx
+++ src/Components/Header/Logo.tsx
@@ -1,7 +1,13 @@
 import { Text } from "@chakra-ui/react";
 export function Logo() {
+
   return (
-    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+    <Text
+      fontSize={['2xl', '3xl']}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
       dashgo
       <Text as="span" ml="1" color="pink.500">
         .
@@ -9,3 +15,5 @@ export function Logo() {
     </Text>
   );
 }
+
+
