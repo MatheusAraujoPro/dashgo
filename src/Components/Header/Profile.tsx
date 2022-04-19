34051f54ad6013cd4db1528ba252e1fc8b26ccb3
Matheus Araujo <matheus.a@havan.com.br>
2022-04-19 17:32:09 -0300

    Header Responsivo

--- src/Components/Header/Profile.tsx
+++ src/Components/Header/Profile.tsx
@@ -3,17 +3,21 @@ import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 interface ProfileProos {
   name: string;
   email: string;
+  showProfileData?: boolean;
 }
 
-export function Profile({ name, email }: ProfileProos) {
+export function Profile({ name, email, showProfileData = true }: ProfileProos) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>{name}</Text>
-        <Text color="gray.300" fontSize="small">
-          {email}
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>{name}</Text>
+          <Text color="gray.300" fontSize="small">
+            {email}
+          </Text>
+        </Box>
+      )}
+
       <Avatar size="md" name="Matheus de Araújo" />
     </Flex>
   );
