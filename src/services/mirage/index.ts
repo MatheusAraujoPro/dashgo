cd09414aaa3bcf3d69c00f7ddd027011a577ff76
Matheus <matheusaraujo.ti10@gmail.com>
2022-04-24 08:40:33 -0300

    Configurando MirageJS com Factories e Seeds

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,50 @@
+import { createServer, Factory, Model } from 'miragejs'
+import { faker } from '@faker-js/faker'
+interface User{
+    name: string,
+    email: string,
+    created_at: string
+}
+
+export function makeServer(){
+    const server = createServer({
+
+        models:{
+            // O meu objeto pode não usar todas as propriedades que possui
+            user: Model.extend<Partial<User>>({})
+        },
+
+        // Popular o banco de dados do Mirage com muitos dados de uma vez
+        factories: {
+            user: Factory.extend({
+                name(i: number){
+                    return `User ${i}`
+                },
+
+                email(){
+                    return faker.internet.email().toLowerCase()
+                },
+
+                createdAt(){
+                    return faker.date.recent(10)
+                }
+            })
+        },
+
+        seeds(server){
+            server.createList('user', 200)
+        },
+
+        routes(){
+            this.namespace = 'api'
+            this.timing = 750 //milisegundos
+            // Shorthands
+            this.get('/users')
+            this.post('/users')
+            
+            //Para evitar conflito de namespace com as API routes do NExt
+            this.namespace = ''   
+            this.passthrough()
+        }
+    })
+}
